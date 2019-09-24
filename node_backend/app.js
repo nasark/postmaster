81683d8d9a0a2120b8ba84dfc4270bed54e8e561
Nasar Khan <nasarak_786@hotmail.com>
2019-09-23 23:44:18 -0400

    added mongodb support

--- node_backend/app.js
+++ node_backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+mongoose.connect('mongodb+srv://Nasar:TO5EJLynfr6DYJxo@socialnetwork-dbnaa.mongodb.net/social-network?retryWrites=true&w=majority')
+.then(() => {
+  console.log("Connected to MongoDB")
+})
+.catch(() => {
+  console.log("Error connecting to MongoDB")
+});
 
 app.use(bodyParser.json());
 
@@ -14,30 +24,23 @@ app.use((req, res, next)=> {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Success'
   });
 });
 
 app.get('/api/posts', (req, res) => {   //localhost:3000/api/posts
-  const posts = [
-    {
-      id: "post_1",
-      title: "First post from server",
-      content: "from server"
-    },
-    {
-      id: "post_2",
-      title: "Second post from server",
-      content: "also from server"
-    }
-  ];
-
-  res.status(200).json({
-    message: "GET request success",
-    posts: posts
+  Post.find()
+  .then(documents =>{
+    res.status(200).json({
+      message: "GET request success",
+      posts: documents
+    });
   });
 })
 
